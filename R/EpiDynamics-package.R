8de61d52c6de5562dd8c924770cd3776e67b852b
oswaldo <oswaldosant@gmail.com>
2015-12-05 19:30:19 -0200

    Selective imports

--- R/EpiDynamics-package.R
+++ R/EpiDynamics-package.R
@@ -3,9 +3,9 @@
 #' \tabular{ll}{
 #' Package: \tab EpiDynamics\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.2\cr
-#' Date: \tab 2014-08-21\cr
-#' Depends: \tab R (>= 3.0.2)\cr
+#' Version: \tab 0.3.0\cr
+#' Date: \tab 2015-12-03\cr
+#' Depends: \tab R (>= 3.2.2)\cr
 #' Imports: \tab deSolve, reshape2, ggplot2, grid\cr
 #' License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr
@@ -19,5 +19,8 @@
 #' @docType package
 #' @title The EpiDynamics Package
 #' @keywords package
-#' @import deSolve reshape2 ggplot2 grid
+#' @import deSolve reshape2 ggplot2
+#' @importFrom grid viewport pushViewport grid.newpage grid.layout
+#' @importFrom stats rnorm runif terms time
+#' @importFrom utils tail
 NULL

73d046866b7a669fdc04491c9231a4665546b2c1
oswaldo <oswaldosant@gmail.com>
2015-12-05 19:24:06 -0200

    SIR: Corrected value for argument `R`

--- R/SIR.R
+++ R/SIR.R
@@ -15,7 +15,7 @@
 #' @examples 
 #' # Parameters and initial conditions.
 #' parameters <- c(beta = 1.4247, gamma = 0.14286)
-#' initials <- c(S = 1 - 1e-06, I = 1e-06, R = 0)
+#' initials <- c(S = 1 - 1e-06, I = 1e-06, R = 1 - (1 - 1e-06 - 1e-06))
 #' 
 #' # Solve and plot.
 #' sir <- SIR(pars = parameters, init = initials, time = 0:70)
